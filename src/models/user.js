20ff92a2a0d00833de7723f2cd5d213c43e20cf0
rosem <romasemenyshyn@gmail.com>
2020-08-04 11:32:31 +0300

    Changed JWT phrase

--- src/models/user.js
+++ src/models/user.js
@@ -70,7 +70,7 @@ userSchema.virtual("tasks", {
 //instance's methods
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, "learningjwt");
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
